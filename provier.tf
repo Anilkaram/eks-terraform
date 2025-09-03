b3ee28b769f3862f10805b62f991852d1eaf5ff3
Anilkumar Karampuri <96483744+Anilkaram@users.noreply.github.com>
2025-09-03 17:39:39 +0530

    Update provier.tf

--- provier.tf
+++ provier.tf
@@ -7,6 +7,19 @@ terraform {
   }
 }
 
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+}
+
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "terraform-ec2-role"
+  type    = "creds"
+}
+
 provider "aws" {
   region = "us-east-1"
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+  token      = data.vault_aws_access_credentials.creds.security_token
 }
